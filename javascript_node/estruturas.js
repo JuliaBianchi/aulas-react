a0c0b61f2076dbf3dccf4036161b426ac3026e6f
Júlia <juliaspaderbianchi@gmail.com>
2022-12-08 14:37:29 -0300

    Conteúdo 05.

---
+++ javascript_node/estruturas.js
@@ -0,0 +1,22 @@
+/*function soma(v1, v2){
+    var resultado = v1 + v2
+    return resultado
+}
+*/
+
+/*const soma = (v1, v2) => v1 + v2
+console.log(soma(5,8));
+*/
+
+/*
+var valor = 10
+while (valor > 0) {
+    console.log(valor);
+    valor = valor -1
+}
+*/
+
+//inicializador, condição, incrementador
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
